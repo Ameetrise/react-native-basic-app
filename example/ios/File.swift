5427d3136bb5701f0bf006f6a6a181e1e4986317
Ameet <ameetrise@gmail.com>
2023-07-14 13:22:46 -0500

    chore: initial commit

---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  BasicAppExample
+//
+
+import Foundation
---
+++ ios/BasicApp.swift
@@ -0,0 +1,8 @@
+@objc(BasicApp)
+class BasicApp: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
